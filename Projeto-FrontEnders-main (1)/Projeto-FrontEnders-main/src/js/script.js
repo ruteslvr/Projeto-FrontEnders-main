4d6c6c8813ddf44a0c58caff54b072a728809dcc
Rute Silveira <114074241+ruteslvr@users.noreply.github.com>
2022-11-11 09:45:50 -0300

    Add files via upload

---
+++ Projeto-FrontEnders-main (1)/Projeto-FrontEnders-main/src/js/script.js
@@ -0,0 +1,109 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
+    }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+let input = document.querySelector(".input");
+let button = document.querySelector(".button");
+button.disabled = true;
+input.addEventListener("change", stateHandle);
+function stateHandle() {
+  if (document.querySelector(".input").value === "") {
+    button.disabled = true; 
+  } else {
+    button.disabled = false;
+  }
+}
+
+function alertEmailSent (){
+  Toastify({
+    text: "Email enviado!",
+    duration: 3000,
+    newWindow: true,
+    gravity: "top", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(190deg, rgba(0,0,0,1) 35%, rgba(11,50,221,1) 100%);",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+}
+
+function alertRegister (){
+  Toastify({
+    text: "Conta criada com sucesso, faça login!",
+    duration: 3000,
+    newWindow: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(190deg, rgba(0,0,0,1) 35%, rgba(11,50,221,1) 100%);",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("mdyMoal2");
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+}
